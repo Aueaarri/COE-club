920da76297151dc1b3d07d6b2757811121910465
kamonnan <kamonnan@kkumail.com>
2024-11-11 03:29:02 +0700

    aboutเกือบfinal ver

--- comclub-NNPart/resources/views/about.blade.php
+++ comclub-NNPart/resources/views/about.blade.php
@@ -1,12 +1,28 @@
 @extends('layout')
+
 @section('content')
-<div class="text">
-    <h1>ComClub!</h1>
-    <h2>for COE Squad!</h2>
-    <h1>One Room at a Time, Please</h1>
-    <h1>Limit of 4 Hours per Booking</h1>
-    <h1>Book Anytime, Rooms Open 24/7!</h1>
-    <h1>Confirm Within 15 Minutes of Arrival</h1>
-</div>
-    
+    <div class="animated-text">
+        Welcome to <span></span>
+    </div>
+
+    <div class="content">
+        <img src="{{ asset('images/game2.png') }}" alt="Icon 1" style="width: 450px; height: 420px;">
+        <div class="text">
+            <h2 class="blue"><i class="fa-solid fa-robot"></i>&nbsp;&nbsp;One Room at a Time, Please</h2>
+            <p> Each booking is for one room at a time, so grab it for yourself or share with your fav people.</p>
+            <br>
+            <h2 class="yellow"><i class="fa-solid fa-clock"></i>&nbsp;&nbsp;&nbsp;Limit of 4 Hours per Booking</h2>
+            <p> To make sure everyone gets a turn, each booking is limited to a max of 4 hours.</p>
+            <br>
+            <h2 class="blue"><i class="fa-regular fa-calendar"></i>&nbsp;&nbsp;&nbsp;Book Anytime, Rooms Open 24/7!</h2>
+            <p> No need to check the clock—our rooms are open 24/7!</p>
+            <br>
+            <h2 class="yellow"><i class="fa-solid fa-circle-check"></i>&nbsp;&nbsp;&nbsp;Confirm Within 15 Minutes of Arrival</h2>
+            <p> Once you arrive, don’t forget to confirm within 15 minutes —better hurry!</p>
+        </div>
+    </div>
+@endsection
+
+@section('title')
+    About
 @endsection
--- comclub-NNPart/resources/views/layout.blade.php
+++ comclub-NNPart/resources/views/layout.blade.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
     <title>@yield('title')</title>
 
     <link rel="stylesheet" href="{{ asset('css/about.css') }}">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -18,11 +19,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jersey+10&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rock+Salt&family=Rubik+Dirt&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet">
+
     <style>
         .navbar-brand {
             font-weight: bold;
             font-family: "Jersey 10", sans-serif;
             font-size: 2rem;
+            color: #2f2f2f;
         }
     </style>
 </head>
@@ -67,9 +75,11 @@
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
     </script>
 
+    <script src="https://kit.fontawesome.com/981340f170.js" crossorigin="anonymous"></script>
 
 
-    <div>
+
+    <div class="body">
         @yield('content')
     </div>
 </body>
